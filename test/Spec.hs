8bd1688528c8acfb2677681502ad0bb54aab105c
Vojtech Knaisl <vknaisl@gmail.com>
2018-12-11 18:32:27 +0100

    Refactor Organization tests to new test structure

--- test/Spec.hs
+++ test/Spec.hs
@@ -22,7 +22,7 @@ import Specs.API.KnowledgeModelAPISpec
 import Specs.API.Level.APISpec
 import Specs.API.Metric.APISpec
 import Specs.API.MigratorAPISpec
-import Specs.API.OrganizationAPISpec
+import Specs.API.Organization.APISpec
 import Specs.API.Package.APISpec
 import Specs.API.Questionnaire.APISpec
 import Specs.API.TokenAPISpec
