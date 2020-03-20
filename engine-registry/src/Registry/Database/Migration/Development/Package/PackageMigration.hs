a0a8b52a72306ec010cf6df2541f01c8687243a5
Vojtech Knaisl <vknaisl@gmail.com>
2020-03-20 17:46:02 +0100

    Fix package development migration in Registry

--- engine-registry/src/Registry/Database/Migration/Development/Package/PackageMigration.hs
+++ engine-registry/src/Registry/Database/Migration/Development/Package/PackageMigration.hs
@@ -12,5 +12,4 @@ runMigration = do
   insertPackage globalPackage
   insertPackage netherlandsPackage
   insertPackage netherlandsPackageV2
-  insertPackage germanyPackage
   logInfo $ msg _CMP_MIGRATION "(Package/Package) ended"
