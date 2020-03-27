207a155347852c7be435c6f2755aeeb3b1cc7ad4
Vojtech Knaisl <vknaisl@gmail.com>
2020-03-27 11:21:37 +0100

    Change Wizard Config Bootstrap Endpoint to server JSON

--- engine-wizard/src/Wizard/Api/Handler/Config/List_Bootstrap_GET.hs
+++ engine-wizard/src/Wizard/Api/Handler/Config/List_Bootstrap_GET.hs
@@ -1,13 +1,10 @@
 module Wizard.Api.Handler.Config.List_Bootstrap_GET where
 
-import Data.Aeson (encode)
-import qualified Data.ByteString.Lazy as LBS
-import qualified Data.ByteString.Lazy.Char8 as LBS_Char
-import Data.Maybe (fromMaybe)
 import Servant
 
 import Shared.Api.Handler.Common
 import Wizard.Api.Handler.Common
+import Wizard.Api.Resource.Config.ClientConfigDTO
 import Wizard.Api.Resource.Config.ClientConfigJM ()
 import Wizard.Model.Context.BaseContext
 import Wizard.Service.Config.ClientConfigService
@@ -15,14 +12,7 @@ import Wizard.Service.Config.ClientConfigService
 type List_Bootstrap_GET
    = "configs"
      :> "bootstrap"
-     :> QueryParam "callback" String
-     :> Get '[ ApplicationJavascript] (Headers '[ Header "x-trace-uuid" String] Javascript)
+     :> Get '[ SafeJSON] (Headers '[ Header "x-trace-uuid" String] ClientConfigDTO)
 
-list_bootstrap_GET :: Maybe String -> BaseContextM (Headers '[ Header "x-trace-uuid" String] Javascript)
-list_bootstrap_GET mCallbackName =
-  runInUnauthService $
-  addTraceUuidHeader =<< do
-    let callbackName = fromMaybe "callback" mCallbackName
-    resDto <- getClientConfig
-    let content = LBS.append (LBS_Char.pack callbackName) (LBS.append "(" (LBS.append (encode resDto) ")"))
-    return . Javascript $ content
+list_bootstrap_GET :: BaseContextM (Headers '[ Header "x-trace-uuid" String] ClientConfigDTO)
+list_bootstrap_GET = runInUnauthService $ addTraceUuidHeader =<< getClientConfig
--- engine-wizard/test/Wizard/Specs/API/Config/List_Bootstrap_GET.hs
+++ engine-wizard/test/Wizard/Specs/API/Config/List_Bootstrap_GET.hs
@@ -4,8 +4,6 @@ module Wizard.Specs.API.Config.List_Bootstrap_GET
 
 import Control.Lens ((^.))
 import Data.Aeson (encode)
-import qualified Data.Text.Lazy as LT
-import Data.Text.Lazy.Encoding (decodeUtf8, encodeUtf8)
 import Network.HTTP.Types
 import Network.Wai (Application)
 import Test.Hspec
@@ -23,10 +21,7 @@ import Wizard.Specs.API.Common
 -- GET /configs/bootstrap
 -- ------------------------------------------------------------------------
 list_bootstrap_GET :: AppContext -> SpecWith Application
-list_bootstrap_GET appContext =
-  describe "GET /configs/bootstrap" $ do
-    test_200 appContext
-    test_200_with_query appContext
+list_bootstrap_GET appContext = describe "GET /configs/bootstrap" $ test_200 appContext
 
 -- ----------------------------------------------------
 -- ----------------------------------------------------
@@ -47,29 +42,9 @@ test_200 appContext =
      -- GIVEN: Prepare expectation
    do
     let expStatus = 200
-    let expHeaders = resCtHeaderJavascript : resCorsHeaders
+    let expHeaders = resCtHeader : resCorsHeaders
     let expDto = toClientConfigDTO (appContext ^. applicationConfig) defaultAppConfig
-    let expBody = encodeUtf8 $ LT.concat ["callback", "(", decodeUtf8 . encode $ expDto, ")"]
-     -- WHEN: Call API
-    response <- request reqMethod reqUrl reqHeaders reqBody
-     -- THEN: Compare response with expectation
-    let responseMatcher =
-          ResponseMatcher {matchHeaders = expHeaders, matchStatus = expStatus, matchBody = bodyEquals expBody}
-    response `shouldRespondWith` responseMatcher
-
--- ----------------------------------------------------
--- ----------------------------------------------------
--- ----------------------------------------------------
-test_200_with_query appContext =
-  it "HTTP 200 OK (with query)" $
-     -- GIVEN: Prepare request
-   do
-    let reqUrl = "/configs/bootstrap?callback=myFunction"
-     -- AND: Prepare expectation
-    let expStatus = 200
-    let expHeaders = resCtHeaderJavascript : resCorsHeaders
-    let expDto = toClientConfigDTO (appContext ^. applicationConfig) defaultAppConfig
-    let expBody = encodeUtf8 $ LT.concat ["myFunction(", decodeUtf8 . encode $ expDto, ")"]
+    let expBody = encode expDto
      -- WHEN: Call API
     response <- request reqMethod reqUrl reqHeaders reqBody
      -- THEN: Compare response with expectation
