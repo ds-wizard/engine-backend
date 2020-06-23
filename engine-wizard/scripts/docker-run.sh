dfee80d4dd8979ddb414484f7ed9b6d5cd6e657a
Marek Suchánek <marek.suchanek@gmail.com>
2020-06-23 16:37:21 +0200

    Add script to load templates

--- engine-wizard/scripts/docker-run.sh
+++ engine-wizard/scripts/docker-run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 #
-# This file is supposed to be used in Docker, it is used to prepare
+# This file is supposed to be used in Docker, it is used to prepare 
 # various optional stuff and then runs engine-wizard (backend)
+
+# Set locales to UTF-8
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+# Template loading
+if [[ $ENABLE_TEMPLATE_LOAD == "1" ]]; then
+    echo "Initiating loading templates from filesystem"
+    (sleep 60 && python3 load-templates.py /application/engine-wizard/templates http://localhost:3000 $SERVICE_TOKEN) &
+fi
 
 # Start engine-wizard
 /application/engine-wizard-bin
