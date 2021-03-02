628114d59681846c85f07d53fea3a6c2e08620f5
Marek Suchánek <marek.suchanek@gmail.com>
2021-03-02 21:20:27 +0100

    Remove script to load templates

--- engine-wizard/scripts/docker-run.sh
+++ engine-wizard/scripts/docker-run.sh
@@ -7,11 +7,5 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-# Template loading
-if [[ $ENABLE_TEMPLATE_LOAD == "1" ]]; then
-    echo "Initiating loading templates from filesystem"
-    (sleep 60 && python3 load-templates.py /application/engine-wizard/templates http://localhost:3000 $SERVICE_TOKEN) &
-fi
-
 # Start engine-wizard
 /application/engine-wizard-bin
