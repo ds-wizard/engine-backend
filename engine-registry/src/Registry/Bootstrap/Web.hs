e309426630ad48e92b2eb422d01d6e4871957075
Vojtech Knaisl <vknaisl@gmail.com>
2022-03-05 09:58:57 +0100

    Print errors even if sentry is disabled

--- engine-registry/src/Registry/Bootstrap/Web.hs
+++ engine-registry/src/Registry/Bootstrap/Web.hs
@@ -65,4 +65,7 @@ createSentryHandler context = do
       let buildVersion = context ^. buildInfoConfig . version
       return $ sentryOnException buildVersion sentryService
     else do
-      return (\_ _ -> return ())
+      return
+        (\_ error -> do
+           print error
+           return ())
