5583abec8266996a2421b11cc05bb92465d03697
Marek Suchánek <marek.suchanek@gmail.com>
2022-05-23 20:24:45 +0200

    [DSW-1385] Update to event metamodel v13

---
+++ engine-wizard-metamodel-migrator/src/Wizard/Metamodel/Migration/Migration0012.hs
@@ -0,0 +1,12 @@
+module Wizard.Metamodel.Migration.Migration0012
+  ( migrateEventValue
+  ) where
+
+import Data.Aeson
+
+import Wizard.Metamodel.Migration.MigrationContext
+
+-- Migration #0012 (KM v12 -> v13)
+-- (Nothing to be migrated, new "value question" types allowed)
+migrateEventValue :: MigrationContext -> Value -> Either String [Value]
+migrateEventValue _ input = Right [input]
