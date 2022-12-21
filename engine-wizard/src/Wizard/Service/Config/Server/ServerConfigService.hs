8c4fec171e5f6e093de4ddf59e530482812dc25d
Vojtech Knaisl <vknaisl@gmail.com>
2022-12-21 17:00:23 +0100

    Apply env variables to server config

--- engine-wizard/src/Wizard/Service/Config/Server/ServerConfigService.hs
+++ engine-wizard/src/Wizard/Service/Config/Server/ServerConfigService.hs
@@ -1,9 +1,13 @@
 module Wizard.Service.Config.Server.ServerConfigService where
 
+import Control.Monad.Reader (liftIO)
 import Data.Yaml (decodeFileEither)
+import System.Environment (lookupEnv)
 
 import Shared.Localization.Messages.Internal
+import Shared.Model.Config.ServerConfig
 import Shared.Model.Error.Error
+import Shared.Util.String
 import Wizard.Model.Config.ServerConfig
 import Wizard.Model.Config.ServerConfigJM ()
 
@@ -11,7 +15,9 @@ getServerConfig :: String -> IO (Either AppError ServerConfig)
 getServerConfig fileName = do
   eConfig <- decodeFileEither fileName
   case eConfig of
-    Right config -> return . validateServerConfig $ config
+    Right config -> do
+      updatedConfig <- applyEnvVariables config
+      return . validateServerConfig $ updatedConfig
     Left error -> return . Left . GeneralServerError . show $ error
 
 validateServerConfig :: ServerConfig -> Either AppError ServerConfig
@@ -32,3 +38,43 @@ validateGeneralServerPort config
   | otherwise = Right config
   where
     port = config.general.serverPort
+
+applyEnvVariables :: ServerConfig -> IO ServerConfig
+applyEnvVariables serverConfig =
+  foldl
+    ( \scIO fn -> do
+        sc <- scIO
+        fn sc
+    )
+    (return serverConfig)
+    envs
+
+applyEnvVariable :: String -> String -> (String -> ServerConfig) -> IO ServerConfig
+applyEnvVariable envVariableName oldValue updateFn = do
+  envVariable <- liftIO $ lookupEnv envVariableName
+  case envVariable of
+    Just envVariable -> do
+      print $ f' "Config: Applying env variable %s" [envVariableName]
+      return . updateFn $ envVariable
+    Nothing -> return $ updateFn oldValue
+
+applyMaybeEnvVariable :: String -> Maybe String -> (Maybe String -> ServerConfig) -> IO ServerConfig
+applyMaybeEnvVariable envVariableName oldValue updateFn = do
+  envVariable <- liftIO $ lookupEnv envVariableName
+  case envVariable of
+    Just envVariable -> do
+      print $ f' "Config: Applying env variable %s" [envVariableName]
+      return . updateFn . Just $ envVariable
+    Nothing -> return $ updateFn oldValue
+
+envs :: [ServerConfig -> IO ServerConfig]
+envs =
+  [ \c -> applyEnvVariable "GENERAL_SECRET" c.general.secret (\x -> c {general = c.general {secret = x}})
+  , \c -> applyEnvVariable "DATABASE_CONNECTION_STRING" c.database.connectionString (\x -> c {database = c.database {connectionString = x}})
+  , \c -> applyEnvVariable "S3_URL" c.s3.url (\x -> c {s3 = c.s3 {url = x}})
+  , \c -> applyEnvVariable "S3_PUBLIC_URL" c.s3.publicUrl (\x -> c {s3 = c.s3 {publicUrl = x}})
+  , \c -> applyEnvVariable "S3_USERNAME" c.s3.username (\x -> c {s3 = c.s3 {username = x}})
+  , \c -> applyEnvVariable "S3_PASSWORD" c.s3.password (\x -> c {s3 = c.s3 {password = x}})
+  , \c -> applyEnvVariable "S3_BUCKET" c.s3.bucket (\x -> c {s3 = c.s3 {bucket = x}})
+  , \c -> applyMaybeEnvVariable "S3_REGION" c.s3.region (\x -> c {s3 = c.s3 {region = x}})
+  ]
