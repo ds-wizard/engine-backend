8810af92108c8ed5eb11d04783bd4d06d828e83a
Vojtech Knaisl <vknaisl@gmail.com>
2023-01-02 14:30:19 +0100

    Allow to load a whole server config from env

---
+++ engine-shared/src/Shared/Model/Config/ServerConfigIM.hs
@@ -0,0 +1,77 @@
+module Shared.Model.Config.ServerConfigIM where
+
+import Control.Monad.Reader (liftIO)
+import System.Environment (lookupEnv)
+
+import Shared.Model.Config.ServerConfig
+import Shared.Util.String
+
+instance FromEnv ServerConfigDatabase where
+  applyEnv serverConfig =
+    applyEnvVariables
+      serverConfig
+      [ \c -> applyEnvVariable "DATABASE_CONNECTION_STRING" c.connectionString (\x -> c {connectionString = x})
+      , \c -> applyEnvVariable "DATABASE_STRIPE_SIZE" c.stripeSize (\x -> c {stripeSize = x})
+      , \c -> applyEnvVariable "DATABASE_CONNECTION_TIMEOUT" c.connectionTimeout (\x -> c {connectionTimeout = x})
+      , \c -> applyEnvVariable "DATABASE_MAX_CONNECTIONS" c.maxConnections (\x -> c {maxConnections = x})
+      ]
+
+instance FromEnv ServerConfigS3 where
+  applyEnv serverConfig =
+    applyEnvVariables
+      serverConfig
+      [ \c -> applyEnvVariable "S3_URL" c.url (\x -> c {url = x} :: ServerConfigS3)
+      , \c -> applyEnvVariable "S3_PUBLIC_URL" c.publicUrl (\x -> c {publicUrl = x})
+      , \c -> applyEnvVariable "S3_USERNAME" c.username (\x -> c {username = x})
+      , \c -> applyEnvVariable "S3_PASSWORD" c.password (\x -> c {password = x})
+      , \c -> applyEnvVariable "S3_BUCKET" c.bucket (\x -> c {bucket = x})
+      , \c -> applyMaybeEnvVariable "S3_REGION" c.region (\x -> c {region = x})
+      ]
+
+instance FromEnv ServerConfigAnalytics where
+  applyEnv serverConfig =
+    applyEnvVariables
+      serverConfig
+      [ \c -> applyEnvVariable "ANALYTICS_ENABLED" c.enabled (\x -> c {enabled = x} :: ServerConfigAnalytics)
+      , \c -> applyEnvVariable "ANALYTICS_EMAIL" c.email (\x -> c {email = x})
+      ]
+
+instance FromEnv ServerConfigSentry where
+  applyEnv serverConfig =
+    applyEnvVariables
+      serverConfig
+      [ \c -> applyEnvVariable "SENTRY_ENABLED" c.enabled (\x -> c {enabled = x} :: ServerConfigSentry)
+      , \c -> applyEnvVariable "SENTRY_DSN" c.dsn (\x -> c {dsn = x})
+      ]
+
+-- --------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------
+class FromEnv config where
+  applyEnv :: config -> IO config
+
+applyEnvVariables :: serverConfig -> [serverConfig -> IO serverConfig] -> IO serverConfig
+applyEnvVariables serverConfig =
+  foldl
+    ( \scIO fn -> do
+        sc <- scIO
+        fn sc
+    )
+    (return serverConfig)
+
+applyEnvVariable :: Read value => String -> value -> (value -> serverConfig) -> IO serverConfig
+applyEnvVariable envVariableName oldValue updateFn = do
+  envVariable <- liftIO $ lookupEnv envVariableName
+  case fmap read envVariable of
+    Just envVariable -> do
+      print $ f' "Config: Applying env variable %s" [envVariableName]
+      return . updateFn $ envVariable
+    Nothing -> return $ updateFn oldValue
+
+applyMaybeEnvVariable :: String -> Maybe String -> (Maybe String -> serverConfig) -> IO serverConfig
+applyMaybeEnvVariable envVariableName oldValue updateFn = do
+  envVariable <- liftIO $ lookupEnv envVariableName
+  case envVariable of
+    Just envVariable -> do
+      print $ f' "Config: Applying env variable %s" [envVariableName]
+      return . updateFn . Just $ envVariable
+    Nothing -> return $ updateFn oldValue
--- engine-wizard/src/Wizard/Service/Config/Server/ServerConfigService.hs
+++ engine-wizard/src/Wizard/Service/Config/Server/ServerConfigService.hs
@@ -1,14 +1,12 @@
 module Wizard.Service.Config.Server.ServerConfigService where
 
-import Control.Monad.Reader (liftIO)
 import Data.Yaml (decodeFileEither)
-import System.Environment (lookupEnv)
 
 import Shared.Localization.Messages.Internal
-import Shared.Model.Config.ServerConfig
+import Shared.Model.Config.ServerConfigIM
 import Shared.Model.Error.Error
-import Shared.Util.String
 import Wizard.Model.Config.ServerConfig
+import Wizard.Model.Config.ServerConfigIM ()
 import Wizard.Model.Config.ServerConfigJM ()
 
 getServerConfig :: String -> IO (Either AppError ServerConfig)
@@ -16,7 +14,7 @@ getServerConfig fileName = do
   eConfig <- decodeFileEither fileName
   case eConfig of
     Right config -> do
-      updatedConfig <- applyEnvVariables config
+      updatedConfig <- applyEnv config
       return . validateServerConfig $ updatedConfig
     Left error -> return . Left . GeneralServerError . show $ error
 
@@ -38,43 +36,3 @@ validateGeneralServerPort config
   | otherwise = Right config
   where
     port = config.general.serverPort
-
-applyEnvVariables :: ServerConfig -> IO ServerConfig
-applyEnvVariables serverConfig =
-  foldl
-    ( \scIO fn -> do
-        sc <- scIO
-        fn sc
-    )
-    (return serverConfig)
-    envs
-
-applyEnvVariable :: String -> String -> (String -> ServerConfig) -> IO ServerConfig
-applyEnvVariable envVariableName oldValue updateFn = do
-  envVariable <- liftIO $ lookupEnv envVariableName
-  case envVariable of
-    Just envVariable -> do
-      print $ f' "Config: Applying env variable %s" [envVariableName]
-      return . updateFn $ envVariable
-    Nothing -> return $ updateFn oldValue
-
-applyMaybeEnvVariable :: String -> Maybe String -> (Maybe String -> ServerConfig) -> IO ServerConfig
-applyMaybeEnvVariable envVariableName oldValue updateFn = do
-  envVariable <- liftIO $ lookupEnv envVariableName
-  case envVariable of
-    Just envVariable -> do
-      print $ f' "Config: Applying env variable %s" [envVariableName]
-      return . updateFn . Just $ envVariable
-    Nothing -> return $ updateFn oldValue
-
-envs :: [ServerConfig -> IO ServerConfig]
-envs =
-  [ \c -> applyEnvVariable "GENERAL_SECRET" c.general.secret (\x -> c {general = c.general {secret = x}})
-  , \c -> applyEnvVariable "DATABASE_CONNECTION_STRING" c.database.connectionString (\x -> c {database = c.database {connectionString = x}})
-  , \c -> applyEnvVariable "S3_URL" c.s3.url (\x -> c {s3 = c.s3 {url = x}})
-  , \c -> applyEnvVariable "S3_PUBLIC_URL" c.s3.publicUrl (\x -> c {s3 = c.s3 {publicUrl = x}})
-  , \c -> applyEnvVariable "S3_USERNAME" c.s3.username (\x -> c {s3 = c.s3 {username = x}})
-  , \c -> applyEnvVariable "S3_PASSWORD" c.s3.password (\x -> c {s3 = c.s3 {password = x}})
-  , \c -> applyEnvVariable "S3_BUCKET" c.s3.bucket (\x -> c {s3 = c.s3 {bucket = x}})
-  , \c -> applyMaybeEnvVariable "S3_REGION" c.s3.region (\x -> c {s3 = c.s3 {region = x}})
-  ]
