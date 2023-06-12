5ea83222e504766220559d66772ce7e08f8b8446
Marek Suchánek <marek.suchanek@gmail.com>
2023-06-12 08:34:13 +0200

    [DSW-1948] Fix squashing edit question events
    Co-authored-by: Michaela Zimmermannová <zimmer.misa@seznam.cz>

--- wizard-server/test/Wizard/Specs/Service/KnowledgeModel/Squash/SquasherSpec.hs
+++ wizard-server/test/Wizard/Specs/Service/KnowledgeModel/Squash/SquasherSpec.hs
@@ -42,16 +42,16 @@ squasherSpec =
         let expEvents =
               [ a_q1'
               , a_ch1'
+              , e_ch1_label_2'
+              , EditQuestionEvent' . EditOptionsQuestionEvent' $ e_q1_text {title = e_q1_title_1.title}
               , a_ref1'
               , a_q2'
               , e_q2_title'
-              , EditQuestionEvent' . EditOptionsQuestionEvent' $ e_q1_text {title = e_q1_title_1.title}
               , a_q3'
               , EditQuestionEvent' . EditOptionsQuestionEvent' $
                   e_q1_title_2 {answerUuids = e_q1_answerUuids_title.answerUuids}
               , e_q1_type'
               , e_ref1_url'
-              , e_ch1_label_2'
               , e_ch1_label_3_day2'
               ]
         -- WHEN:
@@ -83,17 +83,17 @@ squasherSpec =
         let expEvents =
               [ a_q1'
               , a_ch1'
+              , e_ch1_label_2'
+              , EditQuestionEvent' . EditOptionsQuestionEvent' $ e_q1_text {title = e_q1_title_1.title}
               , a_ref1'
               , a_q2'
               , e_q2_title'
-              , EditQuestionEvent' . EditOptionsQuestionEvent' $ e_q1_text {title = e_q1_title_1.title}
               , a_q3'
               , e_q1_answerUuids'
               , e_q1_answerUuids_title'
               , e_q1_title_2'
               , e_q1_type'
               , e_ref1_url'
-              , e_ch1_label_2'
               ]
         -- WHEN:
         let resultEvents = squashSimple sourceEvents
@@ -141,7 +141,7 @@ a_q1' = AddQuestionEvent' . AddOptionsQuestionEvent' $ a_q1
 
 a_q1 =
   AddOptionsQuestionEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0001"
+    { uuid = u' "867841b2-a62d-49cd-b933-fc75dddb6c11"
     , parentUuid = u' "70c0e4f3-7a67-49dd-8043-e504392d7903"
     , entityUuid = u' "1890b807-83e8-4a20-8515-83930cab0001"
     , title = ""
@@ -157,7 +157,7 @@ a_ch1' = AddChoiceEvent' a_ch1
 
 a_ch1 =
   AddChoiceEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0002"
+    { uuid = u' "93bc617f-a407-4c08-8c09-627653f2de68"
     , parentUuid = u' "70c0e4f3-7a67-49dd-8043-e504392d7903"
     , entityUuid = u' "1890b807-83e8-4a20-8515-83930cab0002"
     , aLabel = ""
@@ -170,7 +170,7 @@ e_ch1_label_1' = EditChoiceEvent' e_ch1_label_1
 
 e_ch1_label_1 =
   EditChoiceEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0003"
+    { uuid = u' "b91ac5bf-dfb1-4dfa-a1fc-1d9991c334d0"
     , parentUuid = a_ch1.parentUuid
     , entityUuid = a_ch1.entityUuid
     , aLabel = ChangedValue "Label 1 - 1"
@@ -183,7 +183,7 @@ e_q1_title_1' = EditQuestionEvent' . EditOptionsQuestionEvent' $ e_q1_title_1
 
 e_q1_title_1 =
   EditOptionsQuestionEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0004"
+    { uuid = u' "4023c0d1-8387-4336-89a8-440bd268dd25"
     , parentUuid = a_q1.parentUuid
     , entityUuid = a_q1.entityUuid
     , title = ChangedValue "Question 1 Title - 1"
@@ -202,7 +202,7 @@ a_ref1' = AddReferenceEvent' . AddResourcePageReferenceEvent' $ a_ref1
 
 a_ref1 =
   AddResourcePageReferenceEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0005"
+    { uuid = u' "5d0eddba-ae27-47c2-81f6-ed5c892db884"
     , parentUuid = u' "70c0e4f3-7a67-49dd-8043-e504392d7903"
     , entityUuid = u' "1890b807-83e8-4a20-8515-83930cab0003"
     , shortUuid = ""
@@ -215,7 +215,7 @@ a_q2' = AddQuestionEvent' . AddOptionsQuestionEvent' $ a_q2
 
 a_q2 =
   AddOptionsQuestionEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0006"
+    { uuid = u' "7aac99fe-3710-4a63-9b56-260919029130"
     , parentUuid = u' "70c0e4f3-7a67-49dd-8043-e504392d7903"
     , entityUuid = u' "1890b807-83e8-4a20-8515-83930cab0004"
     , title = ""
@@ -231,7 +231,7 @@ e_q2_title' = EditQuestionEvent' . EditOptionsQuestionEvent' $ e_q2_title
 
 e_q2_title =
   EditOptionsQuestionEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0007"
+    { uuid = u' "d007a145-b1a3-4779-a6fd-2e4b328489a0"
     , parentUuid = a_q2.parentUuid
     , entityUuid = a_q2.entityUuid
     , title = ChangedValue "Question 2 Title"
@@ -250,7 +250,7 @@ e_q1_text' = EditQuestionEvent' . EditOptionsQuestionEvent' $ e_q1_text
 
 e_q1_text =
   EditOptionsQuestionEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0008"
+    { uuid = u' "fa2e6fe0-68af-426d-865d-3cadaf63ce34"
     , parentUuid = a_q1.parentUuid
     , entityUuid = a_q1.entityUuid
     , title = NothingChanged
@@ -269,7 +269,7 @@ a_q3' = AddQuestionEvent' . AddOptionsQuestionEvent' $ a_q3
 
 a_q3 =
   AddOptionsQuestionEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0009"
+    { uuid = u' "f534b150-4794-42f5-afbc-5a78e1e48bf0"
     , parentUuid = u' "70c0e4f3-7a67-49dd-8043-e504392d7903"
     , entityUuid = u' "1890b807-83e8-4a20-8515-83930cab0005"
     , title = ""
@@ -285,7 +285,7 @@ e_q1_answerUuids' = EditQuestionEvent' . EditOptionsQuestionEvent' $ e_q1_answer
 
 e_q1_answerUuids =
   EditOptionsQuestionEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0010"
+    { uuid = u' "b71b8844-3f70-4a16-bb5a-9a6682068cb9"
     , parentUuid = a_q1.parentUuid
     , entityUuid = a_q1.entityUuid
     , title = NothingChanged
@@ -304,7 +304,7 @@ e_q1_answerUuids_title' = EditQuestionEvent' . EditOptionsQuestionEvent' $ e_q1_
 
 e_q1_answerUuids_title =
   EditOptionsQuestionEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0011"
+    { uuid = u' "96b14531-fbfa-43e1-af90-1ddc6ab650f5"
     , parentUuid = a_q1.parentUuid
     , entityUuid = a_q1.entityUuid
     , title = ChangedValue "Question 1 Title - Question Uuids"
@@ -323,7 +323,7 @@ e_q1_title_2' = EditQuestionEvent' . EditOptionsQuestionEvent' $ e_q1_title_2
 
 e_q1_title_2 =
   EditOptionsQuestionEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0012"
+    { uuid = u' "86e3da04-cb08-41dc-b227-6ef4ece27d99"
     , parentUuid = a_q1.parentUuid
     , entityUuid = a_q1.entityUuid
     , title = ChangedValue "Question 1 Title - 2"
@@ -342,7 +342,7 @@ e_q1_type' = EditQuestionEvent' . EditValueQuestionEvent' $ e_q1_type
 
 e_q1_type =
   EditValueQuestionEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0013"
+    { uuid = u' "8ccc509f-e02b-46f5-b3be-1eae32a5ce02"
     , parentUuid = a_q1.parentUuid
     , entityUuid = a_q1.entityUuid
     , title = NothingChanged
@@ -361,7 +361,7 @@ e_ref1_type' = EditReferenceEvent' . EditURLReferenceEvent' $ e_ref1_type
 
 e_ref1_type =
   EditURLReferenceEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0014"
+    { uuid = u' "db18919c-9d75-4178-80fc-189e5a8c2fbb"
     , parentUuid = a_ref1.parentUuid
     , entityUuid = a_ref1.entityUuid
     , url = NothingChanged
@@ -375,7 +375,7 @@ e_ref1_url' = EditReferenceEvent' . EditURLReferenceEvent' $ e_ref1_url
 
 e_ref1_url =
   EditURLReferenceEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0015"
+    { uuid = u' "4b586546-fa4c-4d8a-ab91-513e9483ac4a"
     , parentUuid = a_ref1.parentUuid
     , entityUuid = a_ref1.entityUuid
     , url = ChangedValue "Url"
@@ -389,7 +389,7 @@ e_ch1_label_2' = EditChoiceEvent' e_ch1_label_2
 
 e_ch1_label_2 =
   EditChoiceEvent
-    { uuid = u' "abfcd9db-64df-44c7-96d4-135d543a0016"
+    { uuid = u' "65bb1d5c-f8e5-4ddf-be3b-123024555f43"
     , parentUuid = a_ch1.parentUuid
     , entityUuid = a_ch1.entityUuid
     , aLabel = ChangedValue "Label 1 - 2"
