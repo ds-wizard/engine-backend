c8c14d77218a34c4802cbe8d899e7da9d443d032
Vojtech Knaisl <vknaisl@gmail.com>
2024-04-15 10:04:59 +0200

    Fix bottleneck in metric and indication computations

--- wizard-server/src/Wizard/Service/Report/Evaluator/Indication.hs
+++ wizard-server/src/Wizard/Service/Report/Evaluator/Indication.hs
@@ -2,6 +2,7 @@ module Wizard.Service.Report.Evaluator.Indication (
   computeIndications,
 ) where
 
+import qualified Data.Map.Strict as M
 import qualified Data.UUID as U
 
 import Wizard.Model.Questionnaire.QuestionnaireReply
@@ -11,14 +12,14 @@ import WizardLib.KnowledgeModel.Model.KnowledgeModel.KnowledgeModel
 import WizardLib.KnowledgeModel.Model.KnowledgeModel.KnowledgeModelAccessors
 import WizardLib.KnowledgeModel.Model.KnowledgeModel.KnowledgeModelLenses
 
-computeIndications :: Maybe U.UUID -> KnowledgeModel -> [ReplyTuple] -> Chapter -> [Indication]
+computeIndications :: Maybe U.UUID -> KnowledgeModel -> M.Map String Reply -> Chapter -> [Indication]
 computeIndications mQtnPhase km replies ch =
   [computePhasesAnsweredIndication mQtnPhase km replies ch, computeAnsweredIndication Nothing km replies ch]
 
 -- --------------------------------
 -- PRIVATE
 -- --------------------------------
-computeAnsweredIndication :: Maybe U.UUID -> KnowledgeModel -> [ReplyTuple] -> Chapter -> Indication
+computeAnsweredIndication :: Maybe U.UUID -> KnowledgeModel -> M.Map String Reply -> Chapter -> Indication
 computeAnsweredIndication mQtnPhase km replies ch =
   AnsweredIndication' $
     AnsweredIndication
@@ -26,7 +27,7 @@ computeAnsweredIndication mQtnPhase km replies ch =
       , unansweredQuestions = evaluateChapter 0 1 mQtnPhase km replies ch
       }
 
-computePhasesAnsweredIndication :: Maybe U.UUID -> KnowledgeModel -> [ReplyTuple] -> Chapter -> Indication
+computePhasesAnsweredIndication :: Maybe U.UUID -> KnowledgeModel -> M.Map String Reply -> Chapter -> Indication
 computePhasesAnsweredIndication mQtnPhase km replies ch =
   PhasesAnsweredIndication' $
     PhasesAnsweredIndication
@@ -34,16 +35,16 @@ computePhasesAnsweredIndication mQtnPhase km replies ch =
       , unansweredQuestions = evaluateChapter 0 1 mQtnPhase km replies ch
       }
 
-evaluateChapter :: Int -> Int -> Maybe U.UUID -> KnowledgeModel -> [ReplyTuple] -> Chapter -> Int
+evaluateChapter :: Int -> Int -> Maybe U.UUID -> KnowledgeModel -> M.Map String Reply -> Chapter -> Int
 evaluateChapter found notFound mQtnPhase km replies ch =
   let currentPath = U.toString $ ch.uuid
       qs = getQuestionsForChapterUuid km ch.uuid
    in sum . fmap (evaluateQuestion found notFound mQtnPhase km replies currentPath) $ qs
 
-evaluateQuestion :: Int -> Int -> Maybe U.UUID -> KnowledgeModel -> [ReplyTuple] -> String -> Question -> Int
+evaluateQuestion :: Int -> Int -> Maybe U.UUID -> KnowledgeModel -> M.Map String Reply -> String -> Question -> Int
 evaluateQuestion found notFound mQtnPhase km replies path q' =
   let currentPath = composePathUuid path $ getUuid q'
-   in case getReply replies currentPath of
+   in case M.lookup currentPath replies of
         Just reply -> children currentPath
         Nothing -> isRequiredNow km.phaseUuids (getRequiredPhaseUuid q') mQtnPhase notFound
   where
@@ -58,21 +59,21 @@ evaluateQuestion found notFound mQtnPhase km replies path q' =
         rFound = isRequiredNow km.phaseUuids (getRequiredPhaseUuid q') mQtnPhase found
 
 evaluateOptionsQuestion
-  :: OptionsQuestion -> Int -> Int -> Maybe U.UUID -> KnowledgeModel -> [ReplyTuple] -> String -> Int
+  :: OptionsQuestion -> Int -> Int -> Maybe U.UUID -> KnowledgeModel -> M.Map String Reply -> String -> Int
 evaluateOptionsQuestion q found notFound mQtnPhase km replies path =
-  case getReply replies path of
-    Just (_, Reply {value = AnswerReply {..}}) ->
+  case M.lookup path replies of
+    Just (Reply {value = AnswerReply {..}}) ->
       let currentPath = composePathUuid path aValue
           qs = getQuestionsForAnswerUuid km aValue
        in sum . fmap (evaluateQuestion found notFound mQtnPhase km replies currentPath) $ qs
     _ -> isRequiredNow km.phaseUuids q.requiredPhaseUuid mQtnPhase notFound
 
-evaluateListQuestion :: Int -> Int -> Maybe U.UUID -> KnowledgeModel -> [ReplyTuple] -> String -> ListQuestion -> Int
+evaluateListQuestion :: Int -> Int -> Maybe U.UUID -> KnowledgeModel -> M.Map String Reply -> String -> ListQuestion -> Int
 evaluateListQuestion found notFound mQtnPhase km replies currentPath q =
   let itemQs = getItemTemplateQuestionsForQuestionUuid km $ q.uuid
       items =
-        case getReply replies currentPath of
-          Just (_, Reply {value = ItemListReply {..}}) -> ilValue
+        case M.lookup currentPath replies of
+          Just (Reply {value = ItemListReply {..}}) -> ilValue
           _ -> []
       evaluateQuestion' item =
         fmap (evaluateQuestion found notFound mQtnPhase km replies (composePath currentPath $ U.toString item)) itemQs
@@ -84,10 +85,10 @@ evaluateListQuestion found notFound mQtnPhase km replies currentPath q =
    in current + childrens
 
 evaluateMultiChoiceQuestion
-  :: MultiChoiceQuestion -> Int -> Int -> Maybe U.UUID -> KnowledgeModel -> [ReplyTuple] -> String -> Int
+  :: MultiChoiceQuestion -> Int -> Int -> Maybe U.UUID -> KnowledgeModel -> M.Map String Reply -> String -> Int
 evaluateMultiChoiceQuestion q found notFound mQtnPhase km replies path =
-  case getReply replies path of
-    Just (_, Reply {value = MultiChoiceReply {..}}) ->
+  case M.lookup path replies of
+    Just (Reply {value = MultiChoiceReply {..}}) ->
       if not (null mcValue)
         then isRequiredNow km.phaseUuids q.requiredPhaseUuid mQtnPhase found
         else isRequiredNow km.phaseUuids q.requiredPhaseUuid mQtnPhase notFound
