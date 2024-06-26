1d2fc163939640bfaa3f27b6ffcbd6ea2e58a444
Vojtech Knaisl <vknaisl@gmail.com>
2024-06-26 11:21:49 +0200

    Release 4.6.1

--- wizard-server/src/Wizard/Api/Handler/Swagger/Api.hs
+++ wizard-server/src/Wizard/Api/Handler/Swagger/Api.hs
@@ -132,7 +132,7 @@ swagger =
             s._swaggerInfo
               { _infoTitle = "Wizard API"
               , _infoDescription = Just "API specification for Wizard"
-              , _infoVersion = "4.6.0"
+              , _infoVersion = "4.6.1"
               , _infoLicense =
                   Just $
                     License
