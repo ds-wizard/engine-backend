d036704ad67af580aec27c096223352ff0972ddf
Vojtech Knaisl <vknaisl@gmail.com>
2024-07-08 08:55:17 +0200

    Add support for AWS SES

--- wizard-server/src/Wizard/Database/Migration/Production/Migration_0047_qtnCommentAssignedTo/Migration.hs
+++ wizard-server/src/Wizard/Database/Migration/Production/Migration_0047_qtnCommentAssignedTo/Migration.hs
@@ -14,6 +14,10 @@ meta = MigrationMeta {mmNumber = 47, mmName = "Qtn Comment Assigned To", mmDescr
 
 migrate :: Pool Connection -> LoggingT IO (Maybe Error)
 migrate dbPool = do
+  addAssignedTo dbPool
+  addAwsSES dbPool
+
+addAssignedTo dbPool = do
   let sql =
         "ALTER TABLE questionnaire_comment_thread \
         \  ADD COLUMN assigned_to uuid; \
@@ -25,6 +29,25 @@ migrate dbPool = do
         \  ADD CONSTRAINT questionnaire_comment_thread_assigned_by FOREIGN KEY (assigned_by, tenant_uuid) REFERENCES user_entity (uuid, tenant_uuid); \
         \ALTER TABLE questionnaire_comment_thread \
         \  ADD COLUMN notification_required bool;"
+  let action conn = execute_ conn sql
+  liftIO $ withResource dbPool action
+  return Nothing
+
+addAwsSES dbPool = do
+  let sql =
+        "ALTER TABLE instance_config_mail ALTER COLUMN host DROP NOT NULL; \
+        \ALTER TABLE instance_config_mail ALTER COLUMN security DROP NOT NULL; \
+        \ \
+        \ALTER TABLE instance_config_mail RENAME COLUMN host TO smtp_host; \
+        \ALTER TABLE instance_config_mail RENAME COLUMN port TO smtp_port; \
+        \ALTER TABLE instance_config_mail RENAME COLUMN security TO smtp_security; \
+        \ALTER TABLE instance_config_mail RENAME COLUMN username TO smtp_username; \
+        \ALTER TABLE instance_config_mail RENAME COLUMN password TO smtp_password; \
+        \ \
+        \ALTER TABLE instance_config_mail ADD COLUMN provider TEXT NOT NULL DEFAULT('smtp'); \
+        \ALTER TABLE instance_config_mail ADD COLUMN aws_access_key_id TEXT; \
+        \ALTER TABLE instance_config_mail ADD COLUMN aws_secret_access_key TEXT; \
+        \ALTER TABLE instance_config_mail ADD COLUMN aws_region TEXT;"
   let action conn = execute_ conn sql
   liftIO $ withResource dbPool action
   return Nothing
