ced6ec04de23772afef86a328899991180ab4529
Vojtech Knaisl <vknaisl@gmail.com>
2025-02-28 13:53:51 +0100

    Release 4.16.0

--- registry-server/src/Registry/Api/Handler/Swagger/Api.hs
+++ registry-server/src/Registry/Api/Handler/Swagger/Api.hs
@@ -38,7 +38,7 @@ swagger =
             s._swaggerInfo
               { _infoTitle = "Registry API"
               , _infoDescription = Just "API specification for Registry"
-              , _infoVersion = "4.15.0"
+              , _infoVersion = "4.16.0"
               , _infoLicense =
                   Just $
                     License
--- wizard-server/src/Wizard/Api/Handler/Swagger/Api.hs
+++ wizard-server/src/Wizard/Api/Handler/Swagger/Api.hs
@@ -140,7 +140,7 @@ swagger =
             s._swaggerInfo
               { _infoTitle = "Wizard API"
               , _infoDescription = Just "API specification for Wizard"
-              , _infoVersion = "4.15.0"
+              , _infoVersion = "4.16.0"
               , _infoLicense =
                   Just $
                     License
