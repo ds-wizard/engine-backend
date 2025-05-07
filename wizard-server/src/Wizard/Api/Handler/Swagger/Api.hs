90b97a1d6a8b6c2d1e43315f56f166d028e927d9
Vojtech Knaisl <vknaisl@gmail.com>
2025-05-07 08:00:17 +0200

    Release 4.18.0

--- wizard-server/src/Wizard/Api/Handler/Swagger/Api.hs
+++ wizard-server/src/Wizard/Api/Handler/Swagger/Api.hs
@@ -142,7 +142,7 @@ swagger =
             s._swaggerInfo
               { _infoTitle = "Wizard API"
               , _infoDescription = Just "API specification for Wizard"
-              , _infoVersion = "4.17.1"
+              , _infoVersion = "4.18.0"
               , _infoLicense =
                   Just $
                     License
