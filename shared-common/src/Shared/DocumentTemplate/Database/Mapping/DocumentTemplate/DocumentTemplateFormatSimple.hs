1e92c6960d63fb807af93ff51d8cd80507530409
Vojtech Knaisl <vknaisl@gmail.com>
2026-01-12 09:18:34 +0100

    Remove maybe from format in document list DTO

--- shared-common/src/Shared/DocumentTemplate/Database/Mapping/DocumentTemplate/DocumentTemplateFormatSimple.hs
+++ shared-common/src/Shared/DocumentTemplate/Database/Mapping/DocumentTemplate/DocumentTemplateFormatSimple.hs
@@ -1,7 +1,15 @@
 module Shared.DocumentTemplate.Database.Mapping.DocumentTemplate.DocumentTemplateFormatSimple where
 
 import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.FromRow
 
 import Shared.DocumentTemplate.Model.DocumentTemplate.DocumentTemplateFormatSimple
 
 instance FromRow DocumentTemplateFormatSimple
+
+fieldDocumentTemplateFormatSimple :: RowParser DocumentTemplateFormatSimple
+fieldDocumentTemplateFormatSimple = do
+  uuid <- field
+  name <- field
+  icon <- field
+  return DocumentTemplateFormatSimple {..}
