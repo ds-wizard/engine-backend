d1c59c90e6ad50aa4e2987f45624e9bb57828e79
Vojtech Knaisl <vknaisl@gmail.com>
2026-02-03 09:03:37 +0100

    Release 4.27.0

--- registry-server/src/Registry/Api/Handler/Swagger/Api.hs
+++ registry-server/src/Registry/Api/Handler/Swagger/Api.hs
@@ -40,7 +40,7 @@ swagger =
             s._swaggerInfo
               { _infoTitle = "Registry API"
               , _infoDescription = Just "API specification for Registry"
-              , _infoVersion = "4.26.2"
+              , _infoVersion = "4.27.0"
               , _infoLicense =
                   Just $
                     License
